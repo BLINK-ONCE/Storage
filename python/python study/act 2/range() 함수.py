3090ffda2880ba679b5ac8d0d0976f14bb87b893
BLINK-ONCE <mkmkqoa@gmail.com>
2021-01-29 19:38:36 +0900

    feat: Apply .gitignore

---
+++ python/python study/act 2/range() 함수.py
@@ -0,0 +1,8 @@
+# range(n) # 0부터 n-1까지 range 자료형 반환
+# range(m, n) # m부터 n-1까지 range 자료형 반환
+# range(m, n, s) # m부터 시작하여 s만큼씩 증가시키면서 n-1까지 range자료형 반환
+
+# e.g. of range() function
+print(list(range(5)))
+print(list(range(5, 10)))
+print(list(range(1, 10, 2)))
